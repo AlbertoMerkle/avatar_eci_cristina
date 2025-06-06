d71a2907a9cccbd64ae268522c34d8bc13d4d9e4
AlbertoMerkle <alberto.mico@merkle.com>
2025-06-06 09:37:47 +0200

    Update Cristina_Cluster_Original.py credentials

--- Cristina_Cluster_Original.py
+++ Cristina_Cluster_Original.py
@@ -16,7 +16,8 @@ import google.auth
 
 from google.oauth2 import service_account  
 # Create API client. 
-credentials_1 = service_account.Credentials.from_service_account_info(     st.secrets["gcp_service_account"] )
+credentials_dict = st.secrets["gcp"]
+credentials_1 = service_account.Credentials.from_service_account_info(credentials_dict)
 
 from streamlit_extras.app_logo import add_logo
 add_logo("logos/2022-05_Merkle-logo-color.png", height = 25)
